852e867f10d697785f8214d400fc2a8679272299
LucasTMB <lucas.tmb708@gmail.com>
2023-03-04 20:42:18 -0300

    Continuando com o conversor de velocidade, mas ainda não finalizado

--- src/Speed.java
+++ src/Speed.java
@@ -1,2 +1,91 @@
+import javax.swing.*;
+
 public class Speed {
+    public Speed() {
+        Double value = null;
+        Double result = null;
+
+        try {
+            String[] speed = {
+                    "m/s para km/s",
+                    "m/s para km/h",
+                    "m/s para mm/s",
+                    "m/s para µm/s",
+                    "m/s para mi/s",
+                    "m/s para mph",
+                    "m/s para ft/s",
+                    "km/s para m/s",
+                    "km/s para km/h",
+                    "km/s para mm/s",
+                    "km/s para µm/s",
+                    "km/s para mi/s",
+                    "km/s para mph",
+                    "km/s para ft/s",
+                    "km/h para m/s",
+                    "km/h para km/s",
+                    "km/h para mm/s",
+                    "km/h para µm/s",
+                    "km/h para mi/s",
+                    "km/h para mph",
+                    "km/h para ft/s",
+                    "mm/s para m/s",
+                    "mm/s para km/s",
+                    "mm/s para km/h",
+                    "mm/s para µm/s",
+                    "mm/s para mi/s",
+                    "mm/s para mph",
+                    "mm/s para ft/s",
+                    "µm/s para m/s",
+                    "µm/s para km/s",
+                    "µm/s para km/h",
+                    "µm/s para mm/s",
+                    "µm/s para mi/s",
+                    "µm/s para mph",
+                    "µm/s para ft/s",
+                    "mi/s para m/s",
+                    "mi/s para km/s",
+                    "mi/s para km/h",
+                    "mi/s para mm/s",
+                    "mi/s para µm/s",
+                    "mi/s para mph",
+                    "mi/s para ft/s",
+                    "mph para m/s",
+                    "mph para km/s",
+                    "mph para km/h",
+                    "mph para mm/s",
+                    "mph para µm/s",
+                    "mph para mi/s",
+                    "mph para ft/s",
+                    "ft/s para m/s",
+                    "ft/s para km/s",
+                    "ft/s para km/h",
+                    "ft/s para mm/s",
+                    "ft/s para µm/s",
+                    "ft/s para mi/s",
+                    "ft/s para mph"
+            };
+
+            String speeds = JOptionPane.showInputDialog(null, "Escolha a conversão de medida de velocidade", "Conversor de Velocidade",
+                    JOptionPane.PLAIN_MESSAGE, null, speed, null).toString();
+
+            String input = JOptionPane.showInputDialog("Digite o valor");
+            value = Double.parseDouble(input);
+
+            SpeedConverter sc = new SpeedConverter();
+
+            //ainda não finalizado
+        }
+        catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Você não digitou um número", "ERRO", JOptionPane.ERROR_MESSAGE);
+        }
+        finally {
+            int response = JOptionPane.showConfirmDialog(null, "Deseja realizar outra conversão?");
+            if(response == JOptionPane.YES_OPTION) {
+                Screen screen = new Screen();
+                screen.loop();
+            } else {
+                JOptionPane.showMessageDialog(null, "Finalizando por aqui. Muito obrigado por utilizar!");
+            }
+        }
+    }
 }
--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -28,11 +28,11 @@ public class SpeedConverter {
                 result = value * 1000;
             } else if (toSpeed.equals("µm/s")) {
                 result = value * 1000000;
-            } else if (toSpeed.equals("mps")) {
+            } else if (toSpeed.equals("mi/s")) {
                 result = value / 1609.344;
             } else if (toSpeed.equals("mph")) {
                 result = (value / 1609.344) * 3600;
-            } else if (toSpeed.equals("fps")) {
+            } else if (toSpeed.equals("ft/s")) {
                 result = value * 328084;
             }
             return true;
@@ -50,17 +50,179 @@ public class SpeedConverter {
             if(toSpeed.equals("m/s")) {
                 result = value * 1000;
             } else if (toSpeed.equals("km/h")) {
-                result = value * 3.600;
+                result = value * 3600;
             } else if (toSpeed.equals("mm/s")) {
                 result = value * 1000000;
             } else if (toSpeed.equals("µm/s")) {
                 result = value * 1000000000;
-            } else if (toSpeed.equals("mps")) {
+            } else if (toSpeed.equals("mi/s")) {
                 result = value * 0.621371192;
             } else if (toSpeed.equals("mph")) {
                 result = value * 2.23693629;
-            } else if (toSpeed.equals("fps")) {
+            } else if (toSpeed.equals("ft/s")) {
                 result = value * 3280.839895;
+            }
+            return true;
+        } else {
+            System.out.println("Velocidade de destino inválida.");
+            return false;
+        }
+    }
+
+    public boolean kilometersPerHourTo(double value, String toSpeed) {
+        this.value = value;
+        this.toSpeed = toSpeed;
+
+        if (!toSpeed.equals("km/h")) {
+            if(toSpeed.equals("m/s")) {
+                result = value / 3.6;
+            } else if (toSpeed.equals("km/s")) {
+                result = value / 3600;
+            } else if (toSpeed.equals("mm/s")) {
+                result = (value / 3.6) * 1000;
+            } else if (toSpeed.equals("µm/s")) {
+                result = (value / 3.6) * 1000000;
+            } else if (toSpeed.equals("mi/s")) {
+                result = (value / 3.6) * 0.000621371;
+            } else if (toSpeed.equals("mph")) {
+                result = value * 0.621371;
+            } else if (toSpeed.equals("ft/s")) {
+                result = (value / 3.6) * 3.28084;
+            }
+            return true;
+        } else {
+            System.out.println("Velocidade de destino inválida.");
+            return false;
+        }
+    }
+
+    public boolean millimetersPerSecondTo(double value, String toSpeed) {
+        this.value = value;
+        this.toSpeed = toSpeed;
+
+        if (!toSpeed.equals("mm/s")) {
+            if(toSpeed.equals("m/s")) {
+                result = value / 1000;
+            } else if (toSpeed.equals("km/s")) {
+                result = (value / 1000) / 1000;
+            } else if (toSpeed.equals("km/h")) {
+                result = ((value / 1000) * 3.6) / 1000;
+            } else if (toSpeed.equals("µm/s")) {
+                result = value * 1000;
+            } else if (toSpeed.equals("mi/s")) {
+                result = (value / 1000) * 0.000621371;
+            } else if (toSpeed.equals("mph")) {
+                result = ((value / 1000) * 3.6) * 0.621371;
+            } else if (toSpeed.equals("ft/s")) {
+                result = value / 0.0032808;
+            }
+            return true;
+        } else {
+            System.out.println("Velocidade de destino inválida.");
+            return false;
+        }
+    }
+
+    public boolean micrometersPerSecondTo(double value, String toSpeed) {
+        this.value = value;
+        this.toSpeed = toSpeed;
+
+        if (!toSpeed.equals("µm/s")) {
+            if(toSpeed.equals("m/s")) {
+                result = value * 0.000001;
+            } else if (toSpeed.equals("km/s")) {
+                result = (value * 0.000000001) / 1000;
+            } else if (toSpeed.equals("km/h")) {
+                result = value * 0.0036;
+            } else if (toSpeed.equals("mm/s")) {
+                result = value / 1000;
+            } else if (toSpeed.equals("mi/s")) {
+                result = value * 0.000000000621371;
+            } else if (toSpeed.equals("mph")) {
+                result = value * 0.00223694;
+            } else if (toSpeed.equals("ft/s")) {
+                result = value * 0.0000032808;
+            }
+            return true;
+        } else {
+            System.out.println("Velocidade de destino inválida.");
+            return false;
+        }
+    }
+
+    public boolean milesPerSecondTo(double value, String toSpeed) {
+        this.value = value;
+        this.toSpeed = toSpeed;
+
+        if (!toSpeed.equals("mi/s")) {
+            if(toSpeed.equals("m/s")) {
+                result = value * 1609.344;
+            } else if (toSpeed.equals("km/s")) {
+                result = value * 1.609344;
+            } else if (toSpeed.equals("km/h")) {
+                result = value * 5793.638;
+            } else if (toSpeed.equals("mm/s")) {
+                result = value * 1609344;
+            } else if (toSpeed.equals("µm/s")) {
+                result = (value * 1.609) * Math.pow(10, 9);
+            } else if (toSpeed.equals("mph")) {
+                result = value * 3600;
+            } else if (toSpeed.equals("ft/s")) {
+                result = value * 5280;
+            }
+            return true;
+        } else {
+            System.out.println("Velocidade de destino inválida.");
+            return false;
+        }
+    }
+
+    public boolean milesPerHourTo(double value, String toSpeed) {
+        this.value = value;
+        this.toSpeed = toSpeed;
+
+        if (!toSpeed.equals("mph")) {
+            if(toSpeed.equals("m/s")) {
+                result = value * 0.44704;
+            } else if (toSpeed.equals("km/h")) {
+                result = value * 1.60934;
+            } else if (toSpeed.equals("km/s")) {
+                result = value * 0.00044704;
+            } else if (toSpeed.equals("mm/s")) {
+                result = value * 447040;
+            } else if (toSpeed.equals("µm/s")) {
+                result = (value * 0.44704) * Math.pow(10, 9);
+            } else if (toSpeed.equals("mi/s")) {
+                result = value / 3600;
+            } else if (toSpeed.equals("ft/s")) {
+                result = value * 1.46667;
+            }
+            return true;
+        } else {
+            System.out.println("Velocidade de destino inválida.");
+            return false;
+        }
+    }
+
+    public boolean feetPerSecondTo(double value, String toSpeed) {
+        this.value = value;
+        this.toSpeed = toSpeed;
+
+        if (!toSpeed.equals("ft/s")) {
+            if(toSpeed.equals("m/s")) {
+                result = value * 0.3048;
+            } else if (toSpeed.equals("km/h")) {
+                result = value * 1.09728;
+            } else if (toSpeed.equals("km/s")) {
+                result = value * 0.0003048;
+            } else if (toSpeed.equals("mm/s")) {
+                result = value * 304.8;
+            } else if (toSpeed.equals("µm/s")) {
+                result = value * 304800;
+            } else if (toSpeed.equals("mi/s")) {
+                result = value * 0.000189394;
+            } else if (toSpeed.equals("mph")) {
+                result = value * 0.681818;
             }
             return true;
         } else {
